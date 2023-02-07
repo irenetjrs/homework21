9fae9f751671cbf803e2150ba8e0f0f69071e66f
irenetjrs <irkapidgorodetska@gmail.com>
2023-02-07 17:15:56 +0200

    first task

--- script.js
+++ script.js
@@ -2,5 +2,13 @@ const car = {
    model: 'Ford',
    year: 2020,
    color: 'grey',
+   signal: function(){
+      alert('fa!fa!');
+   },
 }
 console.log(car.color);
+car.color = 'blue';
+console.log(car.color);
+car.type = 'electric';
+console.log(car.type);
+car.signal();
