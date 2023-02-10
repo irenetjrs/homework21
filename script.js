36303175d65d4aa44b1d1f54e0f4c01a4dd35c31
irenetjrs <irkapidgorodetska@gmail.com>
2023-02-10 14:39:18 +0200

    second task

--- script.js
+++ script.js
@@ -12,3 +12,24 @@ console.log(car.color);
 car.type = 'electric';
 console.log(car.type);
 car.signal();
+
+const salaries = {
+   frontend: 12000,
+   backend: 10000,
+   designer: 8000,
+   dayPay(){
+      alert('we must pay salary on Tuesday!');
+   },
+   total(){
+      let sum = 0;
+      for(const key in salaries){
+         if(typeof salaries[key] === 'number'){
+            sum += salaries[key];
+      }
+   }
+   return sum;
+}
+};
+console.log(salaries.total());
+salaries.manager = 7000;
+console.log(salaries.total());
